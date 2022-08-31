f35d5feb1e643911073984a7b0a4034031cbe9ee
sameerkhadka <sameerkhadka3@gmail.com>
2022-08-31 12:16:38 +0545

    redirect app to admin

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     public function index(){
-        return redirect('/admintest');
+        return redirect('/admin');
     }
 }
